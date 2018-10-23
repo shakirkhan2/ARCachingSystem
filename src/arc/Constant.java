3f4255bb7c08b242400d7051f69c1524345c2566
Shakir Khan <shakir@wooplr.com>
2018-10-23 22:12:08 +0530

    Fixed Indentation

--- src/arc/Constant.java
+++ src/arc/Constant.java
@@ -9,7 +9,6 @@ public class Constant {
      * T2, for frequent entries, referenced at least twice.
      * B1, recently evicted from the T1 cache, but are still tracked.
      * B2, similar evicted entries, but evicted from T2.
-
      */
     public enum QueueType {
         T1, B1, T2, B2
